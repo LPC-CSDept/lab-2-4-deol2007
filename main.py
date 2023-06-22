b6d80a8368c5631a4fdbdf98c2405f5ee44ce221
deol2007 <deolsarika9@gmail.com>
2023-06-22 13:01:28 -0700

    Stage Changes

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def main():
 
     original_str = 'Python Programming'
     ##################################################
-    # Comlete your code here
+    # Complete your code here
     ##################################################
+    original_str = "Python Programming"
+    sub1 = original_str.split()[0]
+    sub2 = original_str.split()[1]
+    merged_str = sub2 + " " + sub1
 
     print(sub2)
     print(sub1)
